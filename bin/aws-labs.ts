8fea591d3ca9023e3e81f47764ea7b0894deea6d
para0056 <nick@nparadis.ca>
2023-01-21 13:37:05 -0500

    route53 zone

--- bin/aws-labs.ts
+++ bin/aws-labs.ts
@@ -1,49 +1,45 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { Route53ZoneStack } from '../lib/route53-zone-stack';
 import { CDKContext } from '../type';
-import { EXPORTDECLARATION_TYPES } from '@babel/types';
-import { Errors } from 'aws-cdk-lib/aws-stepfunctions';
+import { Route53ZoneStack } from '../lib/route53-zone-stack';
 
-export const getContext = (app: cdk.App): CDKContext => {
-  const deployParameters: CDKContext = app.node.tryGetContext("deployParameters");
-  const errors =[];
-  for (const [key, value] of Object.entries({ ...deployParameters})) {
-    errors.push(`Config cdk.json context '${key} requires a value.`)
+// Create Stacks
+const createStacks = async () => {
+  try {
+    const app = new cdk.App();
+    const context = getContext(app);
+    const deployParameters = getContext(app)
+    const props: cdk.StackProps = {
+      env: {
+        account: context.accountId,
+        region: context.accountRegion
+      }
+    }
+    const route53Stack = new Route53ZoneStack(app, 'Route53ZoneStack', props, context.route53Stack)
+  } catch (err) {
+    console.error(err);
   }
-
-if (errors.length > 0) {
-  throw new Error(JSON.stringify(errors));
 }
 
-console.log(JSON.stringify(deployParameters, null, 2));
-return { ...deployParameters }
+// Get Context
+export const getContext = (app: cdk.App): CDKContext => {
+  const deployParameters: CDKContext = app.node.tryGetContext("deployParameters");
+  const errors = [];
+  for (const [key, value] of Object.entries({ ...deployParameters, ...deployParameters.route53Stack })) {
+    if (value == null || value === "") {
+      errors.push(`Config cdk.json context '${key} requires a value.`)
+    }
+  }
+
+  if (errors.length > 0) {
+    throw new Error(JSON.stringify(errors));
+  }
+
+  console.log(JSON.stringify(deployParameters, null, 2));
+  return { ...deployParameters }
 };
 
 
-const app = new cdk.App();
-const deployParameters = getContext(app)
+createStacks();
 
-new Route53ZoneStack(app, 'Route53ZoneStack', {
-  "env": {
-    "domainName": deployParameters.domainName,
-    "account": deployParameters.accountId,
-    "region": deployParameters.accountRegion,
-  }, ...deployParameters
-});
-
-
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
--- lib/route53-zone-stack.ts
+++ lib/route53-zone-stack.ts
@@ -1,20 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_route53 }  from 'aws-cdk-lib';
+import { aws_route53 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Route53Context } from "../type";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export interface Route53ZoneProps extends cdk.StackProps {
-  domainName: string;
-}
-
 export class Route53ZoneStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: cdk.StackProps, context: Route53Context) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    const route53Zone = new aws_route53.PublicHostedZone(this, 'HostedZone', {
-      zoneName: props.domainName,
+    new aws_route53.PublicHostedZone(this, 'HostedZone', {
+      zoneName: context.domainName,
     });
   }
 }
